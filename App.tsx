a216d59a19e8fda460461cf760c3029e2ad8b0bf
ungheeya <98697170+ungheeya@users.noreply.github.com>
2022-03-06 03:13:33 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './containers/TodoApp';
+
+function App() {
+  return (
+    <TodoApp />
+  );
+}
+
+export default App;
--- index.tsx
+++ index.tsx
@@ -1,10 +1,22 @@
-import { combineReducers } from "redux";
-import todos from "./todos";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules'
 
-const rootReducer = combineReducers({
-    todos
-});
+const store = createStore(rootReducer);
 
-export default rootReducer;
-export type RootState = ReturnType<typeof rootReducer>;
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
